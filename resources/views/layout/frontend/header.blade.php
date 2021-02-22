98a4ca18d6f8d0b8bb80e9b3b20bca05ddf96897
harezmii <suatcnby06@gmail.com>
2021-02-22 20:56:45 +0300

    proje frontdent kısmı için vue teknolojisine geçildi

--- resources/views/layout/frontend/header.blade.php
+++ resources/views/layout/frontend/header.blade.php
@@ -49,11 +49,12 @@
                 <div class="header-search">
                     <form>
                         <input class="input search-input" type="text" placeholder="Enter your keyword">
-                        <select class="input search-categories">
-                            <option value="0">All Categories</option>
-                            <option value="1">Category 01</option>
-                            <option value="1">Category 02</option>
-                        </select>
+{{--                        <select class="input search-categories">--}}
+{{--                            <option value="0">All Categories</option>--}}
+{{--                            <option value="1">Category 01</option>--}}
+{{--                            <option value="1">Category 02</option>--}}
+{{--                        </select>--}}
+
                         <button class="search-btn"><i class="fa fa-search"></i></button>
                     </form>
                 </div>
